51275ce6dd807673d0fb39eb35aef14e5c6f2122
Andrew Sims <acsims@tuta.io>
2019-03-05 09:35:12 +1100

    Make cursor invisible in the axes-drawing script

--- solve_for_drawing_auto_scaling_axis.rb
+++ solve_for_drawing_auto_scaling_axis.rb
@@ -6,21 +6,23 @@ require 'curses'
 # Curses.setpos(y=lines, x=cols)
 
 Curses.init_screen #initialises curses library 
-nb_lines = Curses.lines #retreive number of lines (height) of user screen 'stdscr'
-nb_cols = Curses.cols ##retreive number of columns (width) of user screen 'stdscr' 
+screen_height = Curses.lines #retreive number of lines (height) of user screen 'stdscr'
+screen_width = Curses.cols ##retreive number of columns (width) of user screen 'stdscr' 
 
-x = Curses.cols / 2 # save coordinate of midpoint of x axis
-y = Curses.lines / 2 # save coordinate of idpoint of y axis
+Curses.curs_set(0) # Make cursor invisible.
+
+x_midpoint = Curses.cols / 2 # save coordinate of midpoint of x axis
+y_midpoint = Curses.lines / 2 # save coordinate of midpoint of y axis
 
 # draw a vertical line from y = 0 to y = stdscr.height, centered on x axis
-(0).upto(nb_lines-1) do |i| #for some reason (nb_lines) without -1 always draws an extra character
-    Curses.setpos(i, x)
+(0).upto(screen_height - 1) do |i| #for some reason (nb_lines) without -1 always draws an extra character
+    Curses.setpos(i, x_midpoint)
     Curses.addstr("|")
 end
 
 # draw a vertical line from x = 0 to x = stdscr.width, centered on y axis
-(0).upto(nb_cols-1) do |i| 
-    Curses.setpos(y, i)
+(0).upto(screen_width - 1) do |i| 
+    Curses.setpos(y_midpoint, i)
     Curses.addstr("-")
 end
 
