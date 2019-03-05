319c89c3df10f193ecc76fc43d5f9574485c24c2
Andrew Sims <acsims@tuta.io>
2019-03-05 12:57:10 +1100

    add method to test that there is one argument specified at CL

--- termino-visual.rb
+++ termino-visual.rb
@@ -1,29 +1,17 @@
-    # TERMINO VISUAL APP: CSV Visualiser for terminal written in Ruby
-# Iteration 1: Minimum Viable Product
-
-
-# 1. Take artificial CSV and print it to screen as scatterplot
-# 2. Command Line Interface and error checking
-# INPUT: Filename(CSV file only else error)
-# OUTPUT: CSV Object (using csv gem)
-# 3. interpretation of the CSV into something renderable (i.e. points) (scaling to screen)
-# IN: CSV file
-# OUT: a set of x,y coordinates ready to be rendered to screen (scaled)
-# 4. rendering itself
-# IN: the coordinates
-# OUT: image on screen; at least the x and y coordinates in the correct positions, perhaps also basic axes
-
-
-# PARSING .CSV FILE:
 
 require 'csv'
 require 'curses'
+require 'descriptive_statistics'
+
+def single_argument_specified?()
+    unless ARGV.length == 1
+        return false
+    end
+    return true
+end
 
 csv_text = File.read("BOMWeatherData.csv")
-
 weather_csv = CSV.parse(csv_text, :headers => true)
-
-
 # GET Data-point A (months):
 def get_months(csv_data)
     array_of_results = []
