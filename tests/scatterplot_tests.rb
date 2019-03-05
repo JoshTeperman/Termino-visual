8a19b7608a1db0530244c87f12dab44bbf2958bd
Andrew Sims <acsims@tuta.io>
2019-03-05 15:24:07 +1100

    sequester classes and error checking in own files

---
+++ tests/scatterplot_tests.rb
@@ -0,0 +1,71 @@
+require 'curses'
+require 'pry'
+require 'csv'
+
+class Scatterplot
+    def initialize(data, screen_width, screen_height)
+        @raw_data = data
+        @screen_width = screen_width
+        @screen_height = screen_height
+        @x_values = get_variable_values(0)
+        @y_values = get_variable_values(1)
+        @transformed_data = produce_array_of_coordinates(@x_values, @y_values)
+        
+    end
+
+    def produce_array_of_coordinates(variable_arr1, variable_arr2)
+    # This method takes in two arrays
+    # It outputs an array with many two-index arrays composed of an X and a Y value.
+    # These correspond to single observations to be plotted.
+        variable_arr1 = scale_to_user_screen_size(variable_arr1, @screen_width)
+        variable_arr2 = scale_to_user_screen_size(variable_arr2, @screen_height)
+        return variable_arr2.zip(variable_arr1)
+    end
+
+    def get_variable_values(variable)
+    # Takes an index to extract either 0th, 1st, column from raw_data
+        variable_array = []
+        @raw_data.each do |row|
+            variable_array << row[variable]
+        end
+        return variable_array
+    end
+
+    def convert_all_values_to_floats(array)
+    # Converts all values in an array to floats
+        return array.map { |value| value.to_f }
+    end
+
+    def convert_all_values_to_integers(array)
+    # Converts all values in an array to integers
+        return array.map { |value| value.to_i }
+    end
+
+    def scale_to_user_screen_size(array, screen_dimension)
+    # This scales an array of variable values to the size of the user screen and makes all
+    # values integers so that they fit on line and column numbers.
+        array = convert_all_values_to_floats(array)
+
+        # Find the ratio between the range of the variable spread and the user's screen
+        var_range = array.max - array.min
+        range_to_screen_ratio = screen_dimension / var_range
+
+        # Expand variable values according to this ratio
+        array = array.map do |variable_value|
+            variable_value *= range_to_screen_ratio
+        end
+
+        return convert_all_values_to_integers(array)
+    end
+end
+
+csv_file = File.open("../BOMWeatherData.csv")
+weather_data = CSV.parse(csv_file)
+
+Curses.init_screen
+screen_height = Curses.lines
+screen_width = Curses.cols
+Curses.close_screen
+
+scatter = Scatterplot.new(weather_data, screen_width, screen_height)
+binding.pry
